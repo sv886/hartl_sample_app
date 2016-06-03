f5e5e2f3e6db1c22ea016bec2643cc282048ebc3
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-06-02 22:33:21 -0500

    add signin and current_user methods in sess helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+
+  def sign_in(user)
+    remember_token = User.new_remember_token #create a new token
+    cookies.permanent[:remember_token] = remember_token #place the raw token in the browser cookies
+    user.update_attribute(:remember_token, User.digest(remember_token)) #save the hashed token to the database
+    self.current_user = user #set the current user equal to the given user
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
 end
