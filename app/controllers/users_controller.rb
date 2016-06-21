71ce6fd9119db273efe1495a7ec1e6aa24f96cab
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-06-21 04:45:09 -0500

    add tests/methods for correct user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def new
     @user = User.new
@@ -11,11 +12,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id]) #handled by before_action
   end
 
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id]) #handled by before_action
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -45,5 +46,10 @@ class UsersController < ApplicationController
 
     def signed_in_user
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
